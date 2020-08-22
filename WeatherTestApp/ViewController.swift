6b4ba67ebb45f336899f775ead96844d860a4e78
majd.deeb <majddeeb91id@gmail.com>
2020-08-22 12:28:14 +0400

    Initial Commit

---
+++ WeatherTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTestApp
+//
+//  Created by Majd Deeb on 22/08/2020.
+//  Copyright © 2020 Majd Deeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
